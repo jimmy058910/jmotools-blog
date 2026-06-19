f01a55737adcfd5bb0f1db100e6e1d3931bc335a
JMoGaming <jimmy058910@gmail.com>
2026-06-19 10:39:37 -0400

    feat: reading time and table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 export default defineConfig({
   site: 'https://blog.jmotools.com',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: { theme: 'night-owl', wrap: true },
+    remarkPlugins: [remarkReadingTime],
   },
 });
